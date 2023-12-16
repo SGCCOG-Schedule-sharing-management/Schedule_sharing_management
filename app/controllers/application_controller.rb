112db6f3fa2bf54c4b488387f5379832a5ed6912
EC2 Default User <ec2-user@ip-172-31-26-134.ap-northeast-1.compute.internal>
2023-12-16 11:51:29 +0000

    Create session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
     case resource
     when Admin
-      admin_path
+      root_path
     when User
       root_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
 
-
-
   # ユーザー用
 # URL /users/sign_in ...
 devise_for :users, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-
+scope module: :public do
+  get 'users/mypage' => 'users#show', as: 'mypage'
+  get 'users/information/edit' => 'users#edit', as: 'edit_information'
+  patch 'users/information' => 'users#update', as: 'update_information'
+  put 'users/information' => 'users#update'
+end 
 
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
+namespace :admin do
+  get 'top' => 'homes#top', as: 'top'
+  
+  
+end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
